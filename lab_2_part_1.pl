a13721af49443afd3e1cf9e5603cbdfbcbc00869
darakuno <130306024+darakuno@users.noreply.github.com>
2024-11-13 20:26:18 +0300

    finished

---
+++ lab_2_part_1.pl
@@ -0,0 +1,27 @@
+union1([], L, L).
+
+union1([X|Xs],Z,Ys) :-
+    member(X, Z),!,
+    union1(Xs, Z, Ys).
+
+union1([X|Xs],Z,[X|Ys]) :-
+    union1(Xs, Z, Ys).
+
+
+intersection1([], L, []).
+
+intersection1([X|Xs],Z,[X|Ys]) :-
+    member(X,Z), !,
+    intersection1(Xs, Z, Ys).
+
+intersection1([X|Xs],Z,Ys) :-
+    intersection1(Xs, Z, Ys).
+
+diff1([], L, []).
+
+diff1([X|Xs],Z,Ys) :-
+    member(X,Z), !,
+    diff1(Xs, Z, Ys).
+
+diff1([X|Xs],Z,[X|Ys]) :-
+    diff1(Xs, Z, Ys).
---
+++ lab_3_part_1.pl
@@ -0,0 +1,30 @@
+dellast([_], []) :- !.
+dellast([X|Xs], [X|Y]):-
+    dellast(Xs, Y).
+
+pref([], [[]]).
+pref(X, [X|L]) :-
+    dellast(X, Y),
+    pref(Y,L).
+
+postf([], [[]]).
+postf(X, [X|L]):-
+    X = [_|Ys],
+    postf(Ys, L).
+
+segment([], [[]]).
+segment(X, [X|L]) :-
+    postf(X, R),
+    dellast(X, X1),
+    segment(X1, L1),
+    append(R, L1, A),
+    list_to_set(A, [X|L]).
+
+rearr([], []).
+rearr([X|L], P) :-
+    rearr(L, L1),
+    insert(X, L1, P).
+
+insert(X, L, [X|L]).
+insert(X, [H|T], [H|P]) :-
+    insert(X, T, P).
