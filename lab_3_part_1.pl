f48f34b7fedbdb2f5863602f620cb601c1b0e07e
darakuno <130306024+darakuno@users.noreply.github.com>
2024-11-19 11:28:52 +0300

    finished 4

--- lab_3_part_1.pl
+++ lab_3_part_1.pl
@@ -28,3 +28,13 @@ rearr([X|L], P) :-
 insert(X, L, [X|L]).
 insert(X, [H|T], [H|P]) :-
     insert(X, T, P).
+
+list_to_set2([],[]).
+
+list_to_set2([X|Xs], Y) :-
+    member(X, Xs),
+    list_to_set2(Xs, Y).
+
+list_to_set2([X|Xs], [X|Y]):-
+    member(X, Xs) \= false,
+    list_to_set2(Xs, Y).
---
+++ lab_4_part_1.pl
@@ -0,0 +1,34 @@
+merge_union([], Y, Y).
+merge_union(X, [], X).
+merge_union([X|Xs], [Y|Ys], [X|Zs]):-
+    X = Y, !,
+    merge_union(Xs, Ys, Zs).
+merge_union([X|Xs], [Y|Ys], [X|Zs]):-
+    X < Y, !,
+    merge_union(Xs, [Y|Ys], Zs).
+merge_union(X, [Y|Ys], [Y|Zs]):-
+    merge_union(X, Ys, Zs).
+
+merge_inter([], Y, []).
+merge_inter(X, [], []).
+merge_inter([X|Xs], [Y|Ys], [X|Zs]):-
+    X = Y, !,
+    merge_inter(Xs, Ys, Zs).
+merge_inter([X|Xs], [Y|Ys], Zs):-
+    merge_inter(Xs,Ys,Zs).
+
+merge_diff([], Y, []).
+merge_diff(X, [], X).
+merge_diff([X|Xs], [Y|Ys], Zs):-
+    X = Y, !,
+    merge_diff(Xs, Ys, Zs).
+merge_diff([X|Xs], [Y|Ys], [X|Zs]):-
+    merge_diff(Xs,[Y|Ys],Zs).
+
+merge_sim_diff([], Y, Y).
+merge_sim_diff(X, [], X).
+merge_sim_diff([X|Xs], [Y|Ys], Zs):-
+    X = Y, !,
+    merge_sim_diff(Xs, Ys, Zs).
+merge_sim_diff([X|Xs], [Y|Ys], [X,Y|Zs]):-
+    merge_sim_diff(Xs,Ys,Zs).
