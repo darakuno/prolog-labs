bef74565baf8c42e5dee494790b4c89b0dea3143
darakuno <daradarakku@gmail.com>
2024-12-02 16:00:15 +0300

    finished 5th

--- haskel/lab_1/lab_5.hs
+++ haskel/lab_1/lab_5.hs
@@ -1,17 +1,25 @@
 union1 :: (Eq a) => [a] -> [a] -> [a]
 union1 [] y = y
-union1 (x:xs) ys
-    | x `elem` ys = union1 xs ys
-    | otherwise = x : union1 xs ys
+union1 (x:xs) y
+    | x `elem` y = union1 xs y
+    | otherwise = x : union1 xs y
 
 intersection1 :: (Eq a) => [a] -> [a] -> [a]
-intersection1 [] _ = []
-intersection1 (x:xs) ys
-    | x `elem` ys = x : intersection1 xs ys
-    | otherwise   = intersection1 xs ys
+intersection1 [] y = []
+intersection1 (x:xs) y
+    | x `elem` y = x : intersection1 xs y
+    | otherwise = intersection1 xs y
+
+diff1 :: (Eq a) => [a] -> [a] -> [a]
+diff1 [] y = []
+diff1 (x:xs) y
+    | x `elem` y = diff1 xs y
+    | otherwise = x : diff1 xs y
+
 
 main :: IO ()
 main = do
-    let set1 = [1, 2, 3, 5]
+    let set1 = [1, 2, 6, 8, 3, 5]
     let set2 = [3, 4, 5, 2]
-    print $ intersection1 set1 set2
+    print $ diff1 set1 set2
+    
