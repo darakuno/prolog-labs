c5a8db622b575a93f0f9573e7216e3cd42355320
darakuno <daradarakku@gmail.com>
2024-12-08 13:39:46 +0300

    finished 6th

--- haskel/lab_1/lab_6.hs
+++ haskel/lab_1/lab_6.hs
@@ -10,22 +10,46 @@ postf1 :: (Eq a) => [a] -> [[a]]
 postf1 [] = [[]]
 postf1 x = x : postf1 (tail x)
 
--- segment :: (Eq a) => [a] -> [[a]]
--- segment [] = [[]]
--- segment x = let
---     postfixes = postf1 x
---     s = dellast x
---     l = segment s
---     all = postfixes ++ l
+--- 3
+-- Собирает все постфиксы для данного префикса
+collectSegments :: (Eq a) => [a] -> [[a]]
+collectSegments p = postf1 p  -- Получаем все постфиксы для текущего префикса
 
--- rearr :: (Eq a) => [a] -> [a]
+-- Генерирует все сегменты: префиксы и соответствующие постфиксы
+generateSegments :: (Eq a) => [[a]] -> [[a]]
+generateSegments [] = []
+generateSegments (p:ps) = collectSegments p ++ generateSegments ps
+
+-- Удаление дубликатов
+listToSet :: Eq a => [a] -> [a]
+listToSet [] = []  -- Базовый случай: пустой список
+listToSet (x:xs)
+    | x `elem` xs = listToSet xs  -- Если x уже есть в хвосте, пропускаем его
+    | otherwise = x : listToSet xs  -- Иначе добавляем x в результат
+    
+-- Основная функция, которая генерирует сегменты
+segment :: (Eq a) => [a] -> [[a]]
+segment xs = listToSet (generateSegments (pref1 xs))
+
+--- 4
+-- Генерация всех перестановок списка
+rearr :: Eq a => [a] -> [[a]]
+rearr [] = [[]]  -- Базовый случай: перестановка пустого списка
+rearr (x:xs) = concatMap (insert x) (rearr xs)  -- Рекурсивно генерируем перестановки
+
+-- Вставка элемента в каждую позицию списка
+insert :: a -> [a] -> [[a]]
+insert x [] = [[x]]  -- Если список пуст, возвращаем список с одним элементом
+insert x (h:t) = (x:(h:t)) : map (h:) (insert x t)  -- Вставляем x в начало и рекурсивно вставляем в остальные позиции
+
 
 main :: IO ()
 main = do
     let set1 = [1, 2, 6, 8, 3, 5]
-    let set2 = [3, 4, 5]
+    let set2 = [1, 2, 3]
     print $ dellast set1
     print $ pref1 set1
     print $ postf1 set1
-    -- print $ segment set2
+    print $ segment set2
+    mapM_ print $ rearr set2
     
